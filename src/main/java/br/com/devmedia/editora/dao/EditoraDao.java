2a49abef70dcd84723a76d57ebec8b074a76284c
Luiz Paro <lg_paro@hotmail.com>
2016-07-18 12:26:46 -0300

    Creating methods to insert Editora with JdbcTemplate.

--- src/main/java/br/com/devmedia/editora/dao/EditoraDao.java
+++ src/main/java/br/com/devmedia/editora/dao/EditoraDao.java
@@ -1,12 +1,35 @@
 package br.com.devmedia.editora.dao;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import br.com.devmedia.editora.entity.Editora;
 
 @Repository
 public class EditoraDao {
     
     @Autowired
     private JdbcTemplate template;
+    
+    public int insert(Editora editora) {
+        String sql = "INSERT INTO editora(razao_social, cidade, email) VALUES (?, ?, ?)";
+        return this.template.update(sql, editora.getRazaoSocial(), editora.getCidade(), editora.getEmail());
+    }
+    
+    public int save(Editora editora) {
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(this.template);
+        insert.setTableName("editora");
+        insert.setColumnNames(Arrays.asList("razao_social", "cidade", "email"));
+        insert.setGeneratedKeyName("id");
+        
+        Number key = insert.executeAndReturnKey(new BeanPropertySqlParameterSource(editora));
+        editora.setId(key.intValue());
+        
+        return key.intValue();
+    }
 }
--- src/test/java/br/com/devmedia/editora/dao/EditoraDaoTest.java
+++ src/test/java/br/com/devmedia/editora/dao/EditoraDaoTest.java
@@ -1,5 +1,7 @@
 package br.com.devmedia.editora.dao;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -7,6 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.devmedia.editora.AppEditora;
+import br.com.devmedia.editora.entity.Editora;
 
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -15,4 +18,22 @@ public class EditoraDaoTest {
 
     @Autowired
     private EditoraDao editoraDao;
+    
+    @Test
+    public void shouldPersistEditora() {
+        Editora editora = new Editora("Editora Sul Ltda.", "Porto Alegre", "contato@ed-sul.com");
+        int updatedRows = this.editoraDao.insert(editora);
+        
+        Assert.assertEquals(1, updatedRows);
+    }
+    
+    @Test
+    public void shouldPersistEditoraGeneratingAnId() {
+        Editora editora = new Editora("Editora Sul Ltda.", "Porto Alegre", "contato@ed-sul.com");
+        int generatedId = this.editoraDao.save(editora);
+        
+        Assert.assertTrue(generatedId > 0);
+        Assert.assertNotNull(editora.getId());
+        Assert.assertEquals(Integer.valueOf(generatedId), editora.getId());
+    }
 }
