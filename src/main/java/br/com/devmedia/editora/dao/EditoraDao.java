09b7a20e282f5aed50956b21f7b0a71d03448781
Luiz Paro <lg_paro@hotmail.com>
2016-07-20 07:52:24 -0300

    Transferring sql statements from EditoraDao to external file.

--- src/main/java/br/com/devmedia/editora/dao/EditoraDao.java
+++ src/main/java/br/com/devmedia/editora/dao/EditoraDao.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -13,14 +15,29 @@ import br.com.devmedia.editora.dao.mapper.EditoraMapper;
 import br.com.devmedia.editora.entity.Editora;
 
 @Repository
+@PropertySource("classpath:sql/editora.xml")
 public class EditoraDao {
     
     @Autowired
     private JdbcTemplate template;
     
+    @Value("${sql.insert}")
+    private String sqlInsert;
+    
+    @Value("${sql.findBy.razaoSocial}")
+    private String sqlFindByRazaoSocial;
+    
+    @Value("${sql.findBy.cidade}")
+    private String sqlFindByCidade;
+    
+    @Value("${sql.findBy.id}")
+    private String sqlFindById;
+    
+    @Value("${sql.findAll}")
+    private String sqlFindAll;
+    
     public int insert(Editora editora) {
-        String sql = "INSERT INTO editora(razao_social, cidade, email) VALUES (?, ?, ?)";
-        return this.template.update(sql, editora.getRazaoSocial(), editora.getCidade(), editora.getEmail());
+        return this.template.update(this.sqlInsert, editora.getRazaoSocial(), editora.getCidade(), editora.getEmail());
     }
     
     public int save(Editora editora) {
@@ -36,18 +53,18 @@ public class EditoraDao {
     }
     
     public Editora findById(int id) {
-        return this.template.queryForObject("SELECT * FROM editora WHERE id = ?", new EditoraMapper(), id);
+        return this.template.queryForObject(this.sqlFindById, new EditoraMapper(), id);
     }
     
     public List<Editora> findByRazaoSocial(String razao) {
-        return this.template.query("SELECT * FROM editora WHERE razao_social like '%' ? '%'", new String[] {razao}, new EditoraMapper());
+        return this.template.query(this.sqlFindByRazaoSocial, new String[] {razao}, new EditoraMapper());
     }
     
     public List<Editora> findByCidade(String cityOne, String cityTwo) {
-        return this.template.query("SELECT * FROM editora WHERE cidade IN (?, ?)", new EditoraMapper(), cityOne, cityTwo);
+        return this.template.query(this.sqlFindByCidade, new EditoraMapper(), cityOne, cityTwo);
     }
     
     public List<Editora> findAll() {
-        return this.template.query("SELECT * FROM editora", new EditoraMapper());
+        return this.template.query(this.sqlFindAll, new EditoraMapper());
     }
 }
