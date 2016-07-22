b1f823adcb12c2364b93c88db7c679ba6042f946
Luiz Paro <lg_paro@hotmail.com>
2016-07-22 07:53:25 -0300

    Creating entity Autor.

---
+++ src/main/java/br/com/devmedia/editora/entity/Autor.java
@@ -0,0 +1,70 @@
+package br.com.devmedia.editora.entity;
+
+public class Autor {
+    private Integer id;
+    private String nome;
+    private String email;
+    private Editora editora;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public void setEditora(Editora editora) {
+        this.editora = editora;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Autor))
+            return false;
+        Autor other = (Autor) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor [id=" + id + ", nome=" + nome + ", email=" + email + ", editora=" + editora + "]";
+    }
+}
--- src/main/resources/sql/delta.sql
+++ src/main/resources/sql/delta.sql
@@ -1,11 +1,58 @@
-CREATE SCHEMA `editora-db` DEFAULT CHARACTER SET utf8 ;
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE `editora-db`.`editora` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema editora-db
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema editora-db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `editora-db` DEFAULT CHARACTER SET utf8 ;
+USE `editora-db` ;
+
+-- -----------------------------------------------------
+-- Table `editora-db`.`editora`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `editora-db`.`editora` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `razao_social` VARCHAR(60) NOT NULL,
   `cidade` VARCHAR(60) NOT NULL,
   `email` VARCHAR(60) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `razao_social_UNIQUE` (`razao_social` ASC),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 563
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `editora-db`.`autor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `editora-db`.`autor` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `id_editora` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `nome_UNIQUE` (`nome` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
+  INDEX `fk_autor_editora_idx` (`id_editora` ASC),
+  CONSTRAINT `fk_autor_editora`
+    FOREIGN KEY (`id_editora`)
+    REFERENCES `editora-db`.`editora` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
