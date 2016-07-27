b61a4a78ed32146a8655cba1ed6bb194a00e381c
Luiz Guilherme Paro <lg_paro@hotmail.com>
2016-07-27 07:53:38 -0300

    Creating method to recover complex objects with Spring JdbcTemplate.

--- src/main/java/br/com/devmedia/editora/dao/LivroDao.java
+++ src/main/java/br/com/devmedia/editora/dao/LivroDao.java
@@ -1,18 +1,31 @@
 package br.com.devmedia.editora.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import br.com.devmedia.editora.entity.Autor;
+import br.com.devmedia.editora.entity.Editora;
 import br.com.devmedia.editora.entity.Livro;
 
 @Repository
+@PropertySource("classpath:sql/livro.xml")
 public class LivroDao {
     
     @Autowired
     private JdbcTemplate template;
+    
+    @Value("${sql.livro.findLivroWithAutores}")
+    private String sqlFindLivroWithAutores;
     
     public Livro save(Livro livro) {
         SimpleJdbcInsert insert = new SimpleJdbcInsert(this.template)
@@ -22,6 +35,39 @@ public class LivroDao {
         
         Number key = insert.executeAndReturnKey(new BeanPropertySqlParameterSource(livro));
         livro.setId(key.intValue());
+        
+        return livro;
+    }
+    
+    public Livro findLivroWithAutores(int id) {
+        Livro livro = null;
+        List<Autor> autores = new ArrayList<>();
+        
+        List<Map<String, Object>> rows = this.template.queryForList(this.sqlFindLivroWithAutores, Objects.requireNonNull(id, "id for Livro cannot be null"));
+        for (Map<String, Object> row : rows) {
+            if(livro == null) {
+                livro = new Livro((Integer)row.get("id"),
+                                  (String)row.get("titulo"),
+                                  (Integer)row.get("edicao"),
+                                  (Integer)row.get("paginas"));
+            }
+            Editora editora = new Editora();
+            editora.setId((Integer) row.get("id_editora"));
+            editora.setRazaoSocial((String) row.get("razao_social"));
+            editora.setCidade((String) row.get("cidade"));
+            editora.setEmail((String) row.get("email_editora"));
+            
+            Autor autor = new Autor();
+            autor.setId((Integer) row.get("id_autor"));
+            autor.setNome((String) row.get("nome"));
+            autor.setEmail((String) row.get("email_autor"));
+            autor.setEditora(editora);
+            
+            autores.add(autor);
+        }
+        if(livro != null) {
+            livro.setAutores(autores);
+        }
         
         return livro;
     }
--- src/test/java/br/com/devmedia/editora/dao/LivroDaoTest.java
+++ src/test/java/br/com/devmedia/editora/dao/LivroDaoTest.java
@@ -1,5 +1,8 @@
 package br.com.devmedia.editora.dao;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,31 +13,71 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.devmedia.editora.AppEditora;
+import br.com.devmedia.editora.entity.Autor;
+import br.com.devmedia.editora.entity.Editora;
 import br.com.devmedia.editora.entity.Livro;
+import br.com.devmedia.editora.entity.LivroAutor;
 
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AppEditora.class)
 public class LivroDaoTest {
-    private Livro livroOne;
-    private Livro livroTwo;
+    private Livro bookOne;
+    private Livro bookTwo;
     
     @Autowired
-    private LivroDao livroDao;
+    private LivroDao bookDao;
+    
+    @Autowired
+    private EditoraDao editoraDao;
+    
+    @Autowired
+    private AutorDao autorDao;
+    
+    @Autowired
+    private LivroAutorDao livroAutorDao;
 
     @Before
     public void setUp() {
-        this.livroOne = new Livro("Learn Spring JdbcTemplate", 1, 168);
-        this.livroTwo = new Livro("Learn Spring Data JPA", 2, 210);
-        this.livroDao.save(livroOne);
-        this.livroDao.save(livroTwo);
+        this.bookOne = new Livro("Learn Spring JdbcTemplate", 1, 168);
+        this.bookTwo = new Livro("Learn Spring Data JPA", 2, 210);
+        this.bookDao.save(bookOne);
+        this.bookDao.save(bookTwo);
     }
 
     @Test
     public void shouldInsertLivro() {
         Livro book = new Livro("Learn Spring MVC", 1, 168);
-        this.livroDao.save(book);
+        this.bookDao.save(book);
         
         Assert.assertNotNull(book.getId());
+    }
+    
+    @Test
+    public void testFindLivroWithAutores() {
+        Editora editoraOne = new Editora("Editora Sul Ltda.", "Porto Alegre", "contato@ed-sul.com");
+        Editora editoraTwo = new Editora("Editora Copacabana Ltda.", "Rio de Janeiro", "contato@ed-copacabana.com");
+        this.editoraDao.save(editoraOne);
+        this.editoraDao.save(editoraTwo);
+        
+        List<Autor> authors = Arrays.asList(new Autor("Luciana da Silva", "lucianas@email.com", editoraOne),
+                                            new Autor("João da Silva", "joaos@email.com", editoraTwo));
+        
+        for (Autor author : authors) {
+            this.autorDao.save(author);
+            
+            LivroAutor bookAuthor = new LivroAutor(this.bookOne.getId(), author.getId());
+            this.livroAutorDao.save(bookAuthor);
+        }
+        
+        Livro bookWithAuthors = this.bookDao.findLivroWithAutores(this.bookOne.getId());
+        
+        Assert.assertNotNull(bookWithAuthors);
+        Assert.assertNotNull(bookWithAuthors.getAutores());
+        Assert.assertFalse(bookWithAuthors.getAutores().isEmpty());
+        
+        for (Autor author : bookWithAuthors.getAutores()) {
+            Assert.assertNotNull(author.getId());
+        }
     }
 }
